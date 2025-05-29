c3cad49f31d9ee004bc63c97d09d7dc5a88aedac
Werlei M Andrade <werlleymendes@hotmail.com>
2025-05-29 17:14:47 -0300

    add novos filtros

--- trocas por fornecedor secundario.sql
+++ trocas por fornecedor secundario.sql
@@ -1,13 +1,23 @@
-SELECT  
+SELECT DISTINCT e.seqpessoa, e.fantasia fornecedor, b.seqproduto, b.desccompleta produto, c.estqtroca estoque_troca, (c.cmultvlrnf * c.estqtroca) vlr_estoque
   FROM consinco.map_familia a
         JOIN consinco.map_produto b
              ON a.seqfamilia = b.seqfamilia
         JOIN consinco.mrl_produtoempresa c
              ON b.seqproduto = c.seqproduto
         JOIN consinco.mlf_nfitem d
-             b.seqproduto = d.seqproduto
+             ON b.seqproduto = d.seqproduto           
         JOIN consinco.ge_pessoa e
              ON d.seqpessoa = e.seqpessoa
+        WHERE d.seqpessoa = 1587 and
+              b.seqproduto IN (SELECT f.seqproduto
+                                 FROM consinco.mlf_nfitem f where f.seqpessoa = 1587) and
+              c.estqtroca <> 0
+              ;
+
+
+
+
+SELECT * FROM consinco.mrl_produtoempresa;
 
 
 
@@ -15,9 +25,3 @@ SELECT
 
 
 
-
-
-
-
-
-SELECT * FROM consinco.MLF_NFITEM;
---
+++ trocas por fornecedor secundario.~sql
@@ -0,0 +1,23 @@
+SELECT  
+  FROM consinco.map_familia a
+        JOIN consinco.map_produto b
+             ON a.seqfamilia = b.seqfamilia
+        JOIN consinco.mrl_produtoempresa c
+             ON b.seqproduto = c.seqproduto
+        JOIN consinco.mlf_nfitem d
+             b.seqproduto = d.seqproduto
+        JOIN consinco.ge_pessoa e
+             ON d.seqpessoa = e.seqpessoa
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT * FROM consinco.MLF_NFITEM;
