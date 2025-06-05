df55760769b40b795653ac3859380f31764b29bd
Werlei M Andrade <werlleymendes@hotmail.com>
2025-06-05 10:44:20 -0300

    criação da view

--- trocas por fornecedor secundario.sql
+++ trocas por fornecedor secundario.sql
@@ -1,29 +1,27 @@
-SELECT DISTINCT c.nroempresa, e.seqpessoa, e.fantasia fornecedor, b.seqproduto, b.desccompleta produto, c.estqtroca "ESTOQUE TROCA", (c.cmultvlrnf * c.estqtroca) "VLR ESTOQUE"
-  FROM consinco.map_familia a
-        JOIN consinco.map_produto b
-             ON a.seqfamilia = b.seqfamilia
-        JOIN consinco.mrl_produtoempresa c
-             ON b.seqproduto = c.seqproduto
-        JOIN consinco.mlf_nfitem d
-             ON b.seqproduto = d.seqproduto           
-        JOIN consinco.ge_pessoa e
-             ON d.seqpessoa = e.seqpessoa
-        JOIN consinco.ge_empresa g
-             ON c.nroempresa = g.nroempresa
-        WHERE d.seqpessoa = 1587 and
-              b.seqproduto IN (SELECT f.seqproduto
-                                 FROM consinco.mlf_nfitem f where f.seqpessoa = 1587) and
-              c.estqtroca <> 0
-              ;
-
-
-
-
-
-
-
-
-
+CREATE OR REPLACE VIEW vw_estoque_troca_fornecedor AS
+SELECT DISTINCT 
+    c.nroempresa,
+    e.seqpessoa,
+    e.fantasia AS fornecedor,
+    b.seqproduto,
+    b.desccompleta AS produto,
+    c.estqtroca AS estoque_troca,
+    (c.cmultvlrnf * c.estqtroca) AS vlr_estoque
+FROM consinco.map_familia a
+JOIN consinco.map_produto b ON a.seqfamilia = b.seqfamilia
+JOIN consinco.mrl_produtoempresa c ON b.seqproduto = c.seqproduto
+JOIN consinco.mlf_nfitem d ON b.seqproduto = d.seqproduto           
+JOIN consinco.ge_pessoa e ON d.seqpessoa = e.seqpessoa
+JOIN consinco.ge_empresa g ON c.nroempresa = g.nroempresa
+WHERE 
+    c.estqtroca <> 0
+    AND b.seqproduto IN (
+        SELECT f.seqproduto
+        FROM consinco.mlf_nfitem f 
+        WHERE f.tipnotafiscal = 'E' and
+             f.seqpessoa = d.seqpessoa
+    );
+    
 
 
 
