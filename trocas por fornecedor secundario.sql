c72e9ba6d8016835056974aece9df009719f704b
Werlei M Andrade <werlleymendes@hotmail.com>
2025-06-06 17:04:01 -0300

    Excluindo lojas da consulta

--- trocas por fornecedor secundario.sql
+++ trocas por fornecedor secundario.sql
@@ -6,7 +6,7 @@ SELECT DISTINCT
     b.seqproduto,
     b.desccompleta AS produto,
     c.estqtroca AS estoque_troca,
-    (c.cmultvlrnf * c.estqtroca) AS vlr_estoque
+    ROUND((c.cmultvlrnf * c.estqtroca),2) AS vlr_estoque
 FROM consinco.map_familia a
 JOIN consinco.map_produto b ON a.seqfamilia = b.seqfamilia
 JOIN consinco.mrl_produtoempresa c ON b.seqproduto = c.seqproduto
@@ -14,7 +14,8 @@ JOIN consinco.mlf_nfitem d ON b.seqproduto = d.seqproduto
 JOIN consinco.ge_pessoa e ON d.seqpessoa = e.seqpessoa
 JOIN consinco.ge_empresa g ON c.nroempresa = g.nroempresa
 WHERE 
-    c.estqtroca <> 0
+    c.estqtroca <> 0 and
+    d.seqpessoa NOT IN (1,2,3,4)
     AND b.seqproduto IN (
         SELECT f.seqproduto
         FROM consinco.mlf_nfitem f 
@@ -22,6 +23,4 @@ WHERE
              f.seqpessoa = d.seqpessoa
     );
     
-
-
 
